f53d0d35078d71ee7383ef6720992f15354571f0
Sohamore <167452259+Sohamore@users.noreply.github.com>
2026-06-01 18:48:13 +0530

    fix: remove unused router prop and import in main.tsx since StartClient automatically hydrades itself

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { StartClient } from "@tanstack/react-start/client";
-import { getRouter } from "./router";
-
-const router = getRouter();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <StartClient router={router} />
+    <StartClient />
   </StrictMode>
 );
